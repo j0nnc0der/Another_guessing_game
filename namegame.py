dbb3fee67f7f877e8abf7640b35d8bc3d644249f
Jon Jiro <40280936+j0nnc0der@users.noreply.github.com>
2018-11-08 22:38:21 +0000

    Add files via upload

---
+++ namegame.py
@@ -0,0 +1,28 @@
+import random
+
+
+print('Hello My name is Jon. What is your name?')
+name = input()
+
+print('Well, ' + name + ', I am thinking of a number between 1 and 20.')
+secretNumber = random.randint(1, 20)
+
+print('DEBUG: Secret number is ' + str(secretNumber)) 
+
+for guessesTaken in range(1, 7):
+     print('Take a guess.')
+     guess = int(input())
+
+     if guess < secretNumber:
+          print('If you guess is too low.')
+     elif guess > secretNumber:
+          print('Your guess is too high')
+     else:
+          break # This condition is for the correct guess!
+
+if guess == secretNumber:
+     print('Good job,' + name + '! You guessed my number in '  + str(guessesTaken) + ' guesses!')
+else:
+     print(' Nope. The number I was thinking of was ' + str(secretNumber))
+
+print (' You took ' + str (guessesTaken) + 'guesses.')
